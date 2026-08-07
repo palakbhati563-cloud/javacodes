3a783d55769055129d7045c53ca4696861fdef89
palakbhati563-cloud <palakbhati563@gmail.com>
2026-08-07 21:14:11 +0530

    counting

---
+++ src/counting.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class counting {
+    public static void main (String[]args){
+        Scanner scan = new Scanner (System.in);
+        int i = 1;
+        while (i <= 20)
+        {
+            System.out.println(i);
+            i= i + 1;
+        }
+
+    }
+}
